6c0ff61016b58374b9441f58836c3b18a05707f3
Dost Mohammad Fahimi <dmfahimiiiii730@gmail.com>
2025-01-18 19:15:00 +0430

    add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
-import { Button } from '@chakra-ui/react'
 function App() {
-
   return (
-    <Button colorScheme='blue'>Button</Button>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
