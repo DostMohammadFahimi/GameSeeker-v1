e7b6a535d6b51c4979787cee18db491926b70a3e
Dost Mohammad Fahimi <dmfahimiiiii730@gmail.com>
2025-02-01 17:30:32 +0430

    clean up the game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ interface Props {
   gameQuery: GameQuery;
 }
 
-const  GameGrid = ({ gameQuery }: Props) => {
+const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGame(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
@@ -18,9 +18,9 @@ const  GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <text className="text-danger-300">{error}</text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={3}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
