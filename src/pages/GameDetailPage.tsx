cc5218511270a6eba18e55ae0c9f23da2a7a6968
Dost Mohammad Fahimi <dmfahimiiiii730@gmail.com>
2025-02-19 08:39:31 +0430

    Fetch game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,20 @@
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import { Box, Heading, Spinner } from "@chakra-ui/react";
+
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw Error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Box>{game.description_raw}</Box>
+    </>
+  );
 };
 
 export default GameDetailPage;
